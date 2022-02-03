11555110fe627f2f498e788fd6a993538c32e83f
ahmadsoran <ahmadsorann@gmail.com>
2022-02-04 01:12:19 +0300

    compunents to all route

---
+++ src/routes/AllCompunents.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import AboutME from '../components/AboutME';
+import Client from '../components/Client';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import Header from '../components/header';
+import Navbar from '../components/Navbar';
+import ProjectSlider from '../components/ProjectSlider';
+
+export default function AllCompunents() {
+  return (
+      <>
+   <Navbar />
+   <Header />
+   <AboutME />
+   <ProjectSlider />
+   <Client />
+   <Contact />
+   <Footer />
+   
+      </>
+   
+    )
+}
