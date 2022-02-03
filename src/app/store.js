9626b14871971b08122c15634e67fe78f4c12cee
ahmadsoran <ahmadsorann@gmail.com>
2022-02-04 01:01:57 +0300

    add like slice reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { portfolioAPI } from "./portfolioAPI";
-
+import loginAuth from '../featrues/userSlice.js'
 
 export const store = configureStore({
   reducer: {
+    loginAuth,
     [portfolioAPI.reducerPath]: portfolioAPI.reducer,
   },
 
 });
 
 setupListeners(store.dispatch);
-
