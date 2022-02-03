52a605c8688d77b5fa375a272129cbdd382d374d
ahmadsoran <ahmadsorann@gmail.com>
2022-02-04 01:13:43 +0300

    bugfixs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import AboutME from './components/AboutME';
-import Client from './components/Client';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Header from './components/header.jsx';
-import Navbar from './components/Navbar';
-import ProjectSlider from './components/ProjectSlider';
+import { Route, Routes } from 'react-router-dom';
+import AllCompunents from './routes/AllCompunents';
+import Login from './admin/login'
+import Page404 from './components/Page404';
+import Dashboard from './admin/Dashboard';
+import ProjectUpload from './admin/admin_compunents/projectUpload';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Header />
-      <AboutME />
-      <ProjectSlider />
-      <Client />
-      <Contact />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<AllCompunents />} />
+        <Route path="/_myAdmin" exact element={<Login />} />
+        <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/dashboard/projectsUpload" exact element={<ProjectUpload />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
     </>
   );
 }
