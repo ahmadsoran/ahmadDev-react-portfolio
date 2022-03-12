b4f10e446947f9c2e76d5227064108e655728dd4
ahmadsoran <ahmadsorann@gmail.com>
2022-03-13 02:03:18 +0300

    edit project section

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
-
+import AhmedImg from '../assets/img/me2.png'
 export default function Posts(props) {
     return (
-        <div className="posts-project">
-            <img src={props.ImgSrc} alt={props.alt} />
-            <h1>{props.title}</h1>
-            <p>{props.des}</p>
-            <a href={props.url} target={'_blank'} rel='noreferrer' style={{ color: 'whitesmoke', }}>see Project</a>
+        <div class="card">
+            <div className="bodyCard">
+
+                <div class="card-header">
+                    <img src={props.ImgSrc} alt={props.alt} />
+                </div>
+                <div class="card-content">
+                    <a href={props.url} target={'_blank'} rel='noreferrer' style={{ color: 'whitesmoke', }}>
+                        <span>see Project</span>
+                    </a>
+                    <h3>{props.title}</h3>
+                    <p>{props.des}</p>
+                </div>
+            </div>
+            <div class="card-footer">
+                <img src={AhmedImg} alt="Ahmed Soran" />
+                <div class="author">
+                    <p>Ahmed Soran </p>
+                    <small>Full-Stack Developer</small>
+                </div>
+            </div>
         </div>
+
     )
 }
